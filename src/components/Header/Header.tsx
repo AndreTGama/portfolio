fdf47a6ec09d61393d45e7cc46a3c57ba990d72d
AndreTGama <andre_gama789@hotmail.com>
2023-04-24 13:15:47 -0300

    start use type.js in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 import * as Style from './Header.style';
 import { SocialMedia } from '../Icons/SocialMedia/SocialMedia';
 import { SocialLinks } from '../../assets/data/profile/SocialMedia'; 
 import { faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 export default function Header(): JSX.Element {
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ['Eu sou:', 'Eu sou: André Toledo Gama', 'Eu sou: Back-end Developer'],
+            typeSpeed: 0,
+            backSpeed: 0,
+            smartBackspace: true,
+            loop: false
+        });
+    
+        return () => {
+          typed.destroy();
+        };
+      }, []);
     return (
         <Style.Container>
             <Style.Box>
@@ -17,7 +33,7 @@ export default function Header(): JSX.Element {
                 </Style.Title>
                 <Style.Role>
                     <Style.BlockRole />
-                    <Style.RoleP>Eu sou André Gama Back-end Developer</Style.RoleP>
+                    <Style.RoleP ref={el} />
                 </Style.Role>
                 <Style.Role>
                     <SocialMedia url={SocialLinks.instagram} iconName={faInstagram} />
