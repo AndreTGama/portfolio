4786773cf464626a7009393238f60fb4b8780d57
AndreTGama <andre_gama789@hotmail.com>
2023-05-22 17:57:47 -0300

    transition 2s in icons

--- src/components/Contact/MoreInformation/Index.style.tsx
+++ src/components/Contact/MoreInformation/Index.style.tsx
@@ -41,6 +41,10 @@ export const SpanTittle = styled.span`
 export const Icons = styled.div`
     color: ${Colors.blue};
     margin: 5px;
+    -webkit-transition: color 2s ease-out;
+    -moz-transition: color 2s ease-out;
+    -o-transition: color 2s ease-out;
+    transition: color 2s ease-out;
     :hover{
         color: ${Colors.blueHover};
         text-shadow: white 0px 0px 20px;
--- src/components/Icons/SocialMedia/SocialMedia.style.tsx
+++ src/components/Icons/SocialMedia/SocialMedia.style.tsx
@@ -4,6 +4,10 @@ import { Colors } from '../../../styles/Colors.style'
 export const Anchor = styled.a`
     color: ${Colors.blue};
     margin: 5px;
+    -webkit-transition: color 2s ease-out;
+    -moz-transition: color 2s ease-out;
+    -o-transition: color 2s ease-out;
+    transition: color 2s ease-out;
     :hover{
         color: ${Colors.blueHover};
         text-shadow: white 0px 0px 20px;
