0f566e30665106fc4014b61259024860fd91b7fd
AndreTGama <andre_gama789@hotmail.com>
2023-05-22 17:49:29 -0300

    resize none in textarea

--- src/components/Inputs/TextArea/Index.style.tsx
+++ src/components/Inputs/TextArea/Index.style.tsx
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const Textarea = styled.textarea`
     margin-bottom: 15px;
-    border: 1px solid black;    
+    border: 1px solid black;
+    resize: none;
 `;
