3bd07ef7c0a6f1feb6b6e0e331a09714c8dc7a0c
AndreTGama <andre_gama789@hotmail.com>
2023-05-22 17:43:39 -0300

    paddin in inputs

--- src/components/Inputs/Text/Index.style.tsx
+++ src/components/Inputs/Text/Index.style.tsx
@@ -3,5 +3,6 @@ import styled from 'styled-components';
 export const Input = styled.input`
     margin-bottom: 15px;
     height: 24px;
-    border: 1px solid black;    
+    border: 1px solid black;
+    padding: 5px;   
 `;
